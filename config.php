2c1b485d63bfa06ab7269345ceabf9ec6bd5b41a
VistaPOWA <hegyi.mokka@gmail.com>
2014-05-18 17:38:44 +0200

    init commit

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+// Change this.
+define('BASE_URL', 'http://vista.bananabox.biz/media');
+
+// What file extensions do you want to look for?
+// Files must be tagged and cannot have spaces in the name.
+$validExtensions = array('mp3');
+
+// Names of your playlists, without the .PHP at the end.
+// See playlists/example.php for an example.
+// Must correspond with your jukebox's playlists list.
+$playlists = array(
+	'bar',  // Bar-specific shit.
+	'jazz',
+	'rock',
+	'emagged'
+);
